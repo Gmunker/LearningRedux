7a30567bde177c74c50c45ee0764e070af859fc7
Greg Munker <gmunker@munkerdesigns.com>
2016-10-10 10:16:04 -0400

    add support for CHANGE_NAME action

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -5,10 +5,27 @@ console.log('Starting redux example');
 
 let reducer = (state = {name: "Anonymous"}, action) => {
   //state = state || {name: "Anonymous"};
-  return state;
+
+  console.log('New Action', action);
+  switch (action.type) {
+    case 'CHANGE_NAME':
+      return {
+        ...state,
+        name: action.name
+      };
+      default:
+        return state;
+  }
 };
 
 
 let store = createStore(reducer);
 let currentState = store.getState();
 console.log('CurrentState', currentState);
+
+store.dispatch({
+  type: 'CHANGE_NAME',
+  name: 'Greg'
+});
+
+console.log('Name should be Greg', store.getState());
--- app/redux-todo-example.jsx
+++ app/redux-todo-example.jsx
@@ -10,11 +10,36 @@ var stateDefault = {
   };
 
 let reducer = (state = stateDefault, action) => {
-  //state = state || {name: "Anonymous"};
-  return state;
+  switch(action.type) {
+    case "CHANGE_SEARCH_TEXT":
+      return {
+        ...state,
+        searchText: action.searchText
+      }
+      default:
+        return state
+  }
 };
 
 
 let store = createStore(reducer);
 let currentState = store.getState();
 console.log('CurrentState', currentState);
+
+store.dispatch({
+  type: "CHANGE_SEARCH_TEXT",
+  searchText: "Dog"
+});
+
+console.log('new searchText should be dog', store.getState());
+
+
+
+/*
+change-searchText
+reduce switch
+
+console.log( new state)
+
+
+*/
